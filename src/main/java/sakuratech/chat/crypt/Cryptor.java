7980e4bcf70cd6b16f485f830b939103f57da5fe
two212 <2662623485@qq.com>
2024-09-20 13:01:40 +0800

    添加Cryptor，测试通过

---
+++ src/main/java/sakuratech/chat/crypt/Cryptor.java
@@ -0,0 +1,41 @@
+package sakuratech.chat.crypt;
+
+
+import java.security.SecureRandom;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+public abstract class Cryptor {
+
+    /**
+     * 加密
+     * @param key		密钥
+     * @param data		加密数据
+     * @return			密文
+     * @throws Exception
+     */
+    public static byte[] encode(byte[] key, byte[] data) throws Exception {
+        SecretKeySpec secretKeySpec = new SecretKeySpec(key, "AES");
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+        return cipher.doFinal(data);
+    }
+
+    /**
+     * 解密
+     * @param key		密钥
+     * @param data		密文
+     * @return			解密后的数据
+     * @throws Exception
+     */
+    public static byte[] decode(byte[] key, byte[] data) throws Exception {
+        SecretKeySpec secretKeySpec = new SecretKeySpec(key, "AES");
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
+        return cipher.doFinal(data);
+    }
+}
